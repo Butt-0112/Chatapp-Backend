8684662e4fe054d20c3a8b571dbcd00a07279694
Butt-0112 <qasimbuttqb360@gmail.com>
2025-01-20 18:51:40 +0500

    Initial Commit

--- db.js
+++ db.js
@@ -2,8 +2,37 @@ const path = require('path')
 const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGODB_URI
 
-const dbConnect = async()=>{
-    console.log(MONGO_URI)
-    await mongoose.connect(MONGO_URI)
-}
-module.exports = dbConnect
+if (!MONGO_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable in .env');
+  }
+  
+  /** 
+   * Global is used here to maintain a single Mongoose connection across hot reloads 
+   * in development. Otherwise, Mongoose will reattempt connections.
+   */
+  let cached = global.mongoose;
+  
+  if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+  }
+  
+  async function dbConnect() {
+    if (cached.conn) {
+      return cached.conn;
+    }
+  
+    if (!cached.promise) {
+      const opts = {
+        bufferCommands: true,
+      };
+  
+      cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+        return mongoose;
+      });
+    }
+  
+    cached.conn = await cached.promise;
+    return cached.conn;
+  }
+  
+  module.exports = dbConnect
