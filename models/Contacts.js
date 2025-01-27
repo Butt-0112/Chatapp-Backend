952381f9d04bbccb5862694d1d642aa582000987
Butt-0112 <qasimbuttqb360@gmail.com>
2025-01-27 23:38:08 +0500

    Initial Commit

---
+++ models/Contacts.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  clerkId: {
+    type:String, // Reference to the contact user
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  username:{
+    type:String,
+    required:true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  addedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const userContactsSchema = new mongoose.Schema({
+  clerkId: {
+    type:String, // Reference to the user who owns these contacts
+    required: true,
+    unique: true,
+  },
+  contacts: [contactSchema], // Array of contacts
+});
+
+const UserContacts = mongoose.model('UserContacts', userContactsSchema);
+
+module.exports = UserContacts;
