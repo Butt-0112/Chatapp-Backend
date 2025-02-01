5044416c54dd07f4915c8a55b18bb587ef9af671
Butt-0112 <qasimbuttqb360@gmail.com>
2025-02-02 01:37:31 +0500

    Contacts CRUD APIs

--- routes/messaging/route.js
+++ routes/messaging/route.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { body, validationResult } = require('express-validator')
+router.get('/',async(req,res)=>{
+    res.send('hello world')
+})
 module.exports = router
