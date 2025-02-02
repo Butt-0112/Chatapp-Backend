002e3492aa0e60db025deac8a705e15e339eb50d
Butt-0112 <qasimbuttqb360@gmail.com>
2025-02-03 00:12:33 +0500

    Delete Message Endpoint created

--- routes/messaging/route.js
+++ routes/messaging/route.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const router=  express.Router()
-const { body, validationResult } = require('express-validator')
-router.get('/',async(req,res)=>{
-    res.send('hello world')
+const { body, validationResult } = require('express-validator');
+const Message = require('../../models/Message');
+router.delete('/deleteMessage',[
+    body('messageId','messageId is required').notEmpty()
+],async(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const {messageId} = req.body
+        await Message.findByIdAndDelete(messageId)
+        res.json({msg:'Message Deleted Successfully!'})
+
+        
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error',error });
+    }
 })
 module.exports = router
