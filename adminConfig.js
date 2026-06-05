f0a83638d221f969e16142eda6e3db5de95e23aa
Butt-0112 <qasimbuttqb360@gmail.com>
2026-06-05 21:10:41 +0500

    admin sdk configuration

--- adminConfig.js
+++ adminConfig.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 require('dotenv').config()
-var serviceAccount = require('./etc/secrets/fir-cloud-messaging-7a490-firebase-adminsdk-wa6wp-1947a643f1.json')
+var serviceAccount = require('/etc/secrets/fir-cloud-messaging-7a490-firebase-adminsdk-wa6wp-1947a643f1.json')
 
 
 admin.initializeApp({
